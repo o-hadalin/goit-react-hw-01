6b822e18c2a06cd923e3967a200b6ac29eb5622f
O. HADALIN <dyblikat777@gmail.com>
2024-12-07 20:59:45 +0200

    Buttons & Task 1 without css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [currentTask, setCurrentTask] = useState("task1");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <nav>
+        <button onClick={() => setCurrentTask("task1")}>Task 1</button>
+        <button onClick={() => setCurrentTask("task2")}>Task 2</button>
+        <button onClick={() => setCurrentTask("task3")}>Task 3</button>
+      </nav>
 
-export default App
+      <div>
+        {currentTask === "task1" && (
+          <Profile
+            name={userData.username}
+            tag={userData.tag}
+            location={userData.location}
+            image={userData.avatar}
+            stats={userData.stats}
+          />
+        )}
+        {currentTask === "task2" && <div>Завдання 2 (Placeholder)</div>}
+        {currentTask === "task3" && <div>Завдання 3 (Placeholder)</div>}
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import "modern-normalize";
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
